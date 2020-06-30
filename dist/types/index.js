29c2da9dc429a40f0c21adc31e2ffbd9b05cdd4d
Kyle Goslan <KyleGoslan@users.noreply.github.com>
2020-06-30 23:25:36 +0100

    Update

--- dist/types/index.js
+++ dist/types/index.js
@@ -1,2 +1,20 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./Unit"), exports);
+__exportStar(require("./CountryCode"), exports);
+__exportStar(require("./Language"), exports);
+__exportStar(require("./QueryType"), exports);
+__exportStar(require("./Constructor"), exports);
+__exportStar(require("./Methods"), exports);
+__exportStar(require("./ThreeHourResponse"), exports);
+__exportStar(require("./CurrentReponse"), exports);
